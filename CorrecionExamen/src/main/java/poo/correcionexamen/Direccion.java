d96b7deab9b631be0c48e5995034648ef3c73dcb
Hillary Malespin Ulloa <80055697+HillaryMalespin@users.noreply.github.com>
2023-01-10 08:42:38 -0600

    Add files via upload

--- CorrecionExamen/src/main/java/poo/correcionexamen/Direccion.java
+++ CorrecionExamen/src/main/java/poo/correcionexamen/Direccion.java
@@ -1,59 +1,34 @@
 package CorrecionExamen.src.main.java.poo.correcionexamen;
 
+
+/**
+ *
+ * @author Hillary
+ */
 public class Direccion {
-    private String provincia;
-    private String canton;
-    private String distrito;
-    private String sennas;
 
-    public Direccion() {
-    }
+    private String provincia,canton,distrito,sennas;
 
-    public Direccion(String provincia, String canton, String distrito, String sennas) {
-        this.provincia = provincia;
-        this.canton = canton;
-        this.distrito = distrito;
-        this.sennas = sennas;
+    public Empleado(String pProvincia, String pCanton, String pDistrito, String pSennas) {
+        this.provincia = pProvincia;
+        this.canton = pCanton;
+        this.distrito = pDistrito;
+        this.sennas = pSennas;
     }
 
     public String getProvincia() {
         return provincia;
     }
 
-    public void setProvincia(String provincia) {
-        this.provincia = provincia;
-    }
-
     public String getCanton() {
         return canton;
-    }
-
-    public void setCanton(String canton) {
-        this.canton = canton;
     }
 
     public String getDistrito() {
         return distrito;
     }
 
-    public void setDistrito(String distrito) {
-        this.distrito = distrito;
-    }
-
     public String getSennas() {
         return sennas;
-    }
-
-    public void setSennas(String sennas) {
-        this.sennas = sennas;
-    }
-
-    @Override
-    public String toString() {
-        return "Direccion de la sucursal:" + '\n' +
-                "provincia: " + provincia + '\n' +
-                ", canton: " + canton + '\n' +
-                ", distrito: " + distrito + '\n' +
-                ", señas: " + sennas + '\n';
     }
 }
